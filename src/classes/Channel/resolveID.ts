9b2dcdee92caa32aeb9453776fc5b5c5a0b559d6
APixel Visuals <apixelvisualsofficial@gmail.com>
2021-03-10 18:56:50 -0800

    Fixed bug with `Channel.resolveID()` not resolving channel objects

--- src/classes/Channel/resolveID.ts
+++ src/classes/Channel/resolveID.ts
@@ -1,10 +1,10 @@
-import { Channel, ChannelResolvable } from "../../internal";
+import { CategoryChannel, Channel, ChannelResolvable, DMChannel, GuildChannel, NewsChannel, StoreChannel, TextChannel, VoiceChannel } from "../../internal";
 import isID from "../../util/isID";
 
 export default function resolveID(channelResolvable: ChannelResolvable): string | undefined {
 
     // Channel
-    if (channelResolvable instanceof Channel) return channelResolvable.id;
+    if ((channelResolvable instanceof Channel) || ((channelResolvable as any) instanceof GuildChannel) || ((channelResolvable as any) instanceof DMChannel) || ((channelResolvable as any) instanceof TextChannel) || ((channelResolvable as any) instanceof VoiceChannel) || ((channelResolvable as any) instanceof CategoryChannel) || ((channelResolvable as any) instanceof NewsChannel) || ((channelResolvable as any) instanceof StoreChannel)) return (channelResolvable as Channel).id;
 
     // Channel ID
     else if (isID(channelResolvable)) return channelResolvable;
