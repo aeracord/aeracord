f900cd1162fe0e78059bbd6f156637d1baad1244
APixel Visuals <apixelvisualsofficial@gmail.com>
2021-07-19 01:50:13 -0700

    Fixed bug with `Member.roles` not having the @everyone role

--- src/classes/Member/dataFromRawData.ts
+++ src/classes/Member/dataFromRawData.ts
@@ -6,7 +6,7 @@ export default function dataFromRawData(rawData: RawMemberData, guildID: string)
     return {
         guildID,
         nickname: rawData.nick || null,
-        roles: rawData.roles,
+        roles: [...rawData.roles, guildID],
         joinedAt: new Date(rawData.joined_at).getTime(),
         premiumSince: rawData.premium_since ? new Date(rawData.premium_since).getTime() : null,
         pending: rawData.pending || false,
